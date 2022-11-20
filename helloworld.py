99627a967599b87e891cf5e26d7ec20f651d98d9
SimiZeiger <117586967+SimiZeiger@users.noreply.github.com>
2022-11-20 11:59:53 -0500

    second commit

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,6 @@
 print("Hello World")
 print(1+1)
 print('ask ' + 'Simi!')
+print("Hello I'm learning python here")
+print("I'm not really sure what to write so 'm just gonna type random...")
+print("7+14")
