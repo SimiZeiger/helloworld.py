733acab58e725c60aa32b8709d4a8947e6677cc2
SimiZeiger <117586967+SimiZeiger@users.noreply.github.com>
2022-11-20 12:23:59 -0500

    fourth commit

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,10 @@
-print("Hello World")
-print(1+1)
-print('ask ' + 'Simi!')
-print("Hello I'm learning python here")
-print("I'm not really sure what to write so 'm just gonna type random...")
-print("7+14")
-print('hey ' + 'SUP!')
+# print("Hello World")
+# print(1+1)
+# print('ask ' + 'Simi!')
+# print("Hello I'm learning python here")
+# print("I'm not really sure what to write so 'm just gonna type random...")
+# print("7+14")
+# print('hey ' + 'SUP!')
+    #Variables
+favFood = 'noodles'
+print(favFood)
